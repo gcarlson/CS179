c1013575be7d2d35e48391c30eae763407491b88
gcarlson <gcarlson@caltech.edu>
2015-05-12 04:58:12 -0700

    First try

---
+++ cs179_set5/cluster_cuda.cu
@@ -0,0 +1,100 @@
+#include <cassert>
+#include <cuda_runtime.h>
+#include "cluster_cuda.cuh"
+
+// This assumes address stores the average of n elements atomically updates
+// address to store the average of n + 1 elements (the n elements as well as
+// val). This might be useful for updating cluster centers.
+// modified from http://stackoverflow.com/a/17401122
+__device__ 
+float atomicUpdateAverage(float* address, int n, float val)
+{
+  int* address_as_i = (int*) address;
+  int old = *address_as_i;
+  int assumed;
+  do {
+    assumed = old;
+    float next_val = (n * __int_as_float(assumed) + val) / (n + 1);
+    old = ::atomicCAS(address_as_i, assumed,
+		      __float_as_int(next_val));
+  } while (assumed != old);
+  return __int_as_float(old);
+}
+
+// computes the distance squared between vectors a and b where vectors have
+// length size and stride stride.
+__device__ 
+float squared_distance(float *a, float *b, int stride, int size) {
+  float dist = 0.0;
+  for (int i=0; i < size; i++) {
+    float diff = a[stride * i] - b[stride * i];
+    dist += diff * diff;
+  }
+  return dist;
+}
+
+/*
+ * Notationally, all matrices are column majors, so if I say that matrix Z is
+ * of size m * n, then the stride in the m axis is 1. For purposes of
+ * optimization (particularly coalesced accesses), you can change the format of
+ * any array.
+ *
+ * clusters is a REVIEW_DIM * k array containing the location of each of the k
+ * cluster centers.
+ *
+ * cluster_counts is a k element array containing how many data points are in 
+ * each cluster.
+ *
+ * k is the number of clusters.
+ *
+ * data is a REVIEW_DIM * batch_size array containing the batch of reviews to
+ * cluster. Note that each review is contiguous (so elements 0 through 49 are
+ * review 0, ...)
+ *
+ * output is a batch_size array that contains the index of the cluster to which
+ * each review is the closest to.
+ *
+ * batch_size is the number of reviews this kernel must handle.
+ */
+__global__
+void sloppyClusterKernel(float *clusters, int *cluster_counts, int k, 
+                          float *data, int *output, int batch_size) {
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    float best, tmp;
+    int best_cluster;
+    while (index < batch_size) {
+        best_cluster = 0;
+        best = squared_distance(clusters[0], data[index], REVIEW_DIM, 1);
+        for (int i = 1; i < k; i++) {
+            tmp = squared_distance(clusters[i * REVIEW_DIM], 
+                    data[index * REVIEW_DIM], REVIEW_DIM, 1);
+            if (tmp < best) {
+                best_cluster = i;
+                best = tmp;
+            }
+        }
+        cluster_counts[best_cluster]++;
+        output[index] = best_cluster;
+        for (int j = 0; j < REVIEW_DIM; j++) {
+            atomicUpdateAverage(clusters[REVIEW_DIM * best_cluster + j], 
+                    cluster_counts[best_cluster], data[REVIEW_DIM * index + j]);
+        }
+    }
+}
+
+
+void cudaCluster(float *clusters, int *cluster_counts, int k,
+		 float *data, int *output, int batch_size, 
+		 cudaStream_t stream) {
+  int block_size = (batch_size < 1024) ? batch_size : 1024;
+
+  // grid_size = CEIL(batch_size / block_size)
+  int grid_size = (batch_size + block_size - 1) / block_size;
+  int shmem_bytes = 0;
+
+  sloppyClusterKernel<<<
+    block_size, 
+    grid_size, 
+    shmem_bytes, 
+    stream>>>(clusters, cluster_counts, k, data, output, batch_size);
+}
